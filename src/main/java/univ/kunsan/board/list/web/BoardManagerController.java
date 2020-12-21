8f5baba90699a3699331f73807fc34742ed5d396
junhojava <junho9341@gmail.com>
2020-12-21 16:33:15 +0900

    add comment

--- src/main/java/univ/kunsan/board/list/web/BoardManagerController.java
+++ src/main/java/univ/kunsan/board/list/web/BoardManagerController.java
@@ -6,12 +6,21 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import univ.kunsan.board.list.service.dto.BoardDTO;
+
 @RestController
 public class BoardManagerController {
     
+    /**
+     * search board list
+     * 
+     * @return
+     */
     @GetMapping("/boardList")
     public Map<String, Object> getList()
     {
+        BoardDTO boardDTO = new BoardDTO();
+
         Map<String, Object> boardMap = new HashMap<String, Object>();
 
         boardMap.put("bbsId", "BBSMSTR_AAAAAAAAAAAA");
