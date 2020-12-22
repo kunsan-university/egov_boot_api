3b44d4777eb6ac0c66e4fd94c52a5ca0cf5d80af
junhojava <junho9341@gmail.com>
2020-12-22 15:19:56 +0900

    organized import

--- src/main/java/univ/kunsan/board/list/service/BoardManagerServiceImpl.java
+++ src/main/java/univ/kunsan/board/list/service/BoardManagerServiceImpl.java
@@ -1,8 +1,6 @@
 package univ.kunsan.board.list.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
