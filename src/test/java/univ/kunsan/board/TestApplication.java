1794a745a170f67d1a1403f2ec33adc27202b425
junhojava <junho9341@gmail.com>
2020-12-23 17:06:53 +0900

    board article insert test upload

--- src/test/java/univ/kunsan/board/TestApplication.java
+++ src/test/java/univ/kunsan/board/TestApplication.java
@@ -1,6 +1,6 @@
 package univ.kunsan.board;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -21,11 +21,11 @@ public class TestApplication
     @Autowired
     private MockMvc mvc;
 
+    MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
+
     @Test
     public void testUpdateBoardArticle() throws Exception
     {
-        MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
-
         info.add("bbsId", "BBSMSTR_AAAAAAAAAAAA");
         info.add("lastUpdusrId", "USRCNFRM_00000000000");
         info.add("ntceBgnde", "20201222");
@@ -39,6 +39,37 @@ public class TestApplication
         info.add("replyLc", "0");
 
         mvc.perform(put("/boardArticle").params(info))
+           .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testInsertBoardArticle() throws Exception
+    {
+        info.add("atchFileId","");
+        info.add("bbsId","BBSMSTR_AAAAAAAAAAAA");
+        info.add("frstRegisterId","USRCNFRM_00000000000");
+        info.add("frstRegisterPnttm","");
+        info.add("lastUpdusrId","");
+        info.add("lastUpdusrPnttm","");
+        info.add("ntceBgnde","10000101");
+        info.add("ntceEndde","99991231");
+        info.add("ntcrId","");
+        info.add("ntcrNm","");
+        info.add("nttCn","werht");
+        info.add("nttId","0");
+        info.add("nttNo","0");
+        info.add("nttSj","asdfxzcvbfedsh");
+        info.add("parnts","0");
+        info.add("password","");
+        info.add("inqireCo","0");
+        info.add("replyAt","N");
+        info.add("replyLc","0");
+        info.add("sortOrdr","0");
+        info.add("useAt","");
+        info.add("ntceEnddeView","");
+        info.add("ntceBgndeView","");
+
+        mvc.perform(post("/boardArticle").params(info))
            .andExpect(status().isOk());
     }
 }
