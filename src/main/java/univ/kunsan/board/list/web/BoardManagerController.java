61f651b720875a215055769c936456c12ac35fca
junhojava <junho9341@gmail.com>
2020-12-23 16:02:48 +0900

    add board article method upload

--- src/main/java/univ/kunsan/board/list/web/BoardManagerController.java
+++ src/main/java/univ/kunsan/board/list/web/BoardManagerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,7 +57,26 @@ public class BoardManagerController
         // 7. lastUpdater user set
         // 8. set board attribute(ntcrNm, password, Nttcn)
 
-        boardManagerService.updateBoardArticle(board);
+        try
+        {
+            boardManagerService.updateBoardArticle(board);
+        }
+        catch (Exception e) 
+        {
+            return "failure";
+        }
+
+        return "success";
+    }
+
+    @PostMapping("boardArticle")
+    public String insertBoardArticle(Board board)
+    {
+        // user auth check
+        // file check
+        // check reply or parent
+
+        boardManagerService.insertBoardArticle(board);
 
         return "success";
     }
