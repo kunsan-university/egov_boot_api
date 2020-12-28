dbad2191448489926e52a600efa3dcbe33729de6
junhojava <junho9341@gmail.com>
2020-12-28 20:06:19 +0900

    add cors webconfig

---
+++ src/main/java/univ/kunsan/board/config/WebConfig.java
@@ -0,0 +1,15 @@
+package univ.kunsan.board.config;
+
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
