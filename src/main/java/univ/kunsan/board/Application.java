9bf268789fe403edcfdf95ac2937ba9ebcc6c17c
junhojava <junho9341@gmail.com>
2020-12-28 19:59:03 +0900

    add cors annotation

--- src/main/java/univ/kunsan/board/Application.java
+++ src/main/java/univ/kunsan/board/Application.java
@@ -2,10 +2,12 @@ package univ.kunsan.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class Application
 {
+    @CrossOrigin(origins = "http://localhost:3000")
     public static void main(String... args)
     {
         SpringApplication.run(Application.class, args);
